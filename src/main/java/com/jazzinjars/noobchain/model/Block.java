cd05c4d4eb8e53be98f3493fa3dc6503fb57e1c1
Héctor Ardila López <hectorardila@gmail.com>
2018-05-04 14:59:54 +0200

    Add 'processTransaction' method to the Block

--- src/main/java/com/jazzinjars/noobchain/model/Block.java
+++ src/main/java/com/jazzinjars/noobchain/model/Block.java
@@ -2,17 +2,26 @@ package com.jazzinjars.noobchain.model;
 
 import com.jazzinjars.noobchain.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     private String hash;
     private String previousHash;
+    private String merkleRoot;
+    private ArrayList<Transaction> transactions = new ArrayList<Transaction>();  //our data will be an array of Transactions
     private String data;    //our data will be a simple message
     private long timeStamp; //as number of milliseconds since 1/1/1970
     private int nonce;
 
     //Block Constructor
+    public Block(String previousHash) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
     public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
@@ -22,17 +31,33 @@ public class Block {
 
     //Calculate hash based on blocks contents
     public String calculateHash() {
-        String calculatedHash = StringUtil.applySHA256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+        String calculatedHash = StringUtil.applySHA256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot);
         return calculatedHash;
     }
 
     public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
         String target = StringUtil.getDifficultyString(difficulty); //Create a block with difficulty 0
         while (!hash.substring(0, difficulty).equals(target)) {
             nonce++;
             this.hash = calculateHash();
         }
         System.out.println("Block Mined!!!" + hash);
+    }
+
+    //Add Transactions to this Block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore
+        if (transaction == null) return false;
+        if (previousHash != "0") {
+            if (!transaction.processTransaction()) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block!");
+        return true;
     }
 
     public String getHash() {
@@ -42,4 +67,13 @@ public class Block {
     public String getPreviousHash() {
         return this.previousHash;
     }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public String getMerkleRoot() {
+        return merkleRoot;
+    }
+
 }
